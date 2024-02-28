97678c5f174ab5251486093cec82086e5bcf8782
hammadShamir <hammadshamir7@gmail.com>
2024-02-28 16:27:12 +0500

    Minor Fixes

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -2,7 +2,7 @@ import ServiceCard from "@/elements/ServiceCard"
 
 const Service = () => {
     return (
-        <section className='bg-DeepNavy-blue mx-auto px-4 sm:px-6 lg:px-16 p-8 flex flex-col gap-y-8 items-center'>
+        <section className='bg-light-gray mx-auto px-4 sm:px-6 lg:px-16 p-8 flex flex-col gap-y-8 items-center'>
             <h2
                 className="mt-60 bg-gradient-to-r from-DeepNavy-blue via-blue-500 to-blue-700 bg-clip-text text-5xl font-extrabold text-transparent xl:text-6xl 2xl:text-7xl"
             >
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,13 +7,13 @@ import { Autoplay } from 'swiper/modules';
 const Skills = () => {
     return (
         <div className='relative mx-auto px-4 sm:px-6 lg:px-16'>
-            <div className='bg-DeepNavy-blue p-6 max-w-screen-lg w-full absolute left-1/2 top-[-50px] translate-x-[-50%] shadow-sm shadow-light-gray z-10 rounded-lg'>
+            <div className='bg-DeepNavy-blue p-6 max-w-screen-lg w-full absolute left-1/2 top-[-50px] translate-x-[-50%] shadow-md shadow-DeepNavy-blue z-10 rounded-lg'>
                 <h2
                     className="self-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-5xl font-extrabold text-transparent xl:text-6xl 2xl:text-7xl text-center"
                 >
                     Skills
                 </h2>
-                <div className='relative mt-6'>
+                <div className='relative mt-8'>
 
                     <Swiper
                         slidesPerView={1}
@@ -37,48 +37,40 @@ const Skills = () => {
                         modules={[Autoplay]}
                         style={{ margin: "0px" }}
                     >
-                        <SwiperSlide className=''>
-                            <div className='w-52 h-52 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                        <SwiperSlide className='cursor-pointer'>
+                            <div className='w-40 h-40 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
                                 <div className='bg-DeepNavy-blue h-full rounded-full flex flex-col gap-2 justify-center items-center text-light-gray'>
                                     <Image className='max-w-20' src={'/img/skills/react.png'} width={200} height={200} alt='React Icon' />
                                     <h3 className='text-2xl font-bold'>React</h3>
                                 </div>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className=''>
-                            <div className='w-52 h-52 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                        <SwiperSlide className='cursor-pointer'>
+                            <div className='w-40 h-40 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
                                 <div className='bg-DeepNavy-blue h-full rounded-full flex flex-col gap-2 justify-center items-center text-light-gray'>
                                     <Image className='max-w-20' src={'/img/skills/react.png'} width={200} height={200} alt='React Icon' />
                                     <h3 className='text-2xl font-bold'>React</h3>
                                 </div>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className=''>
-                            <div className='w-52 h-52 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                        <SwiperSlide className='cursor-pointer'>
+                            <div className='w-40 h-40 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
                                 <div className='bg-DeepNavy-blue h-full rounded-full flex flex-col gap-2 justify-center items-center text-light-gray'>
                                     <Image className='max-w-20' src={'/img/skills/react.png'} width={200} height={200} alt='React Icon' />
                                     <h3 className='text-2xl font-bold'>React</h3>
                                 </div>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className=''>
-                            <div className='w-52 h-52 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                        <SwiperSlide className='cursor-pointer'>
+                            <div className='w-40 h-40 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
                                 <div className='bg-DeepNavy-blue h-full rounded-full flex flex-col gap-2 justify-center items-center text-light-gray'>
                                     <Image className='max-w-20' src={'/img/skills/react.png'} width={200} height={200} alt='React Icon' />
                                     <h3 className='text-2xl font-bold'>React</h3>
                                 </div>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className=''>
-                            <div className='w-52 h-52 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-                                <div className='bg-DeepNavy-blue h-full rounded-full flex flex-col gap-2 justify-center items-center text-light-gray'>
-                                    <Image className='max-w-20' src={'/img/skills/react.png'} width={200} height={200} alt='React Icon' />
-                                    <h3 className='text-2xl font-bold'>React</h3>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className=''>
-                            <div className='w-52 h-52 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                        <SwiperSlide className='cursor-pointer'>
+                            <div className='w-40 h-40 mx-auto rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
                                 <div className='bg-DeepNavy-blue h-full rounded-full flex flex-col gap-2 justify-center items-center text-light-gray'>
                                     <Image className='max-w-20' src={'/img/skills/react.png'} width={200} height={200} alt='React Icon' />
                                     <h3 className='text-2xl font-bold'>React</h3>
