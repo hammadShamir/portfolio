188229d8c67faec1c36f650d6088ad828bc1a511
hammadShamir <hammadshamir7@gmail.com>
2024-03-01 18:24:15 +0500

    Fixed spaces in mobile menu links

--- src/elements/MobileMenu.tsx
+++ src/elements/MobileMenu.tsx
@@ -18,7 +18,7 @@ const MobileMenu: React.FC<menu> = ({ menu, setMenu }) => {
     return (
         <section className={`bg-DeepNavy-blue w-full absolute top-0 left-0 overflow-hidden ms-auto transition-all ease-in-out duration-500 flex justify-center items-center z-[-1] ${menu ? " h-screen" : 'h-0 '} `}>
             <nav aria-label="Global" className="relative flex justify-center items-center w-full">
-                <ul className="flex flex-col items-center gap-y-12 text-3xl text-light-gray w-full">
+                <ul className="flex flex-col items-center gap-y-6 text-3xl text-light-gray w-full">
                     <li className="transition-all hover:bg-light-gray hover:text-DeepNavy-blue cursor-pointer py-4 w-full text-center"
                         onClick={() => setMenu(!menu)}
                     >
