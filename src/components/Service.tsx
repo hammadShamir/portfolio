38967bf0957a478ae8325ca452b1356201deeaa7
hammadShamir <hammadshamir7@gmail.com>
2024-03-01 18:30:55 +0500

    Adjust space for last service card

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -26,10 +26,10 @@ const Service = () => {
                     <h3 className="text-xl font-bold z-10 group-hover:text-light-gray font-bold text-center">Content Management Systems (CMS)</h3>
                     <p className="text-center text-base z-10 group-hover:text-light-gray font-bold">Simplifying content management through user-friendly custom CMS solutions.</p>
                 </div>
-                <div className="relative bg-DeepNavy-blue text-light-gray p-2 rounded flex flex-col justify-center items-center gap-y-4 shadow-lg shadow-DeepNavy-blue before:absolute before:top-0 before:right-0 before:w-10 before:h-0 hover:before:h-full hover:before:w-full before:bg-gradient-to-r before:from-indigo-500 before:via-purple-500 before:to-pink-500 cursor-pointer before:transition-all before:ease-in-out before:duration-500 group" >
+                <div className="relative bg-DeepNavy-blue text-light-gray p-6 rounded flex flex-col justify-center items-center gap-y-4 shadow-lg shadow-DeepNavy-blue before:absolute before:top-0 before:right-0 before:w-10 before:h-0 hover:before:h-full hover:before:w-full before:bg-gradient-to-r before:from-indigo-500 before:via-purple-500 before:to-pink-500 cursor-pointer before:transition-all before:ease-in-out before:duration-500 group" >
                     <i className="bg-light-gray text-DeepNavy-blue h-12 w-12 flex justify-center items-center rounded-full z-10 group-hover:text-light-gray font-bold"><BsDatabaseFillCheck className="text-4xl text-DeepNavy-blue group-hover:text-DeepNavy-blue" /></i>
                     <h3 className="text-xl font-bold z-10 group-hover:text-light-gray font-bold text-center">API Development & Database Design</h3>
-                    <p className="text-center text-base z-10 group-hover:text-light-gray font-bold">Optimizing applications with robust API development and efficient database design.</p>
+                    <p className="text-center text-base z-10 group-hover:text-light-gray font-bold">Optimizing applications with robust APIs development and efficient database design.</p>
                 </div>
             </div>
         </section>
