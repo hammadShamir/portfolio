e4e6a19a0c90edb9269f7bb95efa832c4e705549
hammadShamir <hammadshamir7@gmail.com>
2024-03-02 16:34:18 +0500

    Service Data added

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,8 +1,22 @@
+import { ReactElement } from "react";
+
+import { MdWeb } from "react-icons/md";
+import { BsDatabaseFillCheck } from "react-icons/bs";
+import { MdAddBusiness } from "react-icons/md";
+import { SiWebpack } from "react-icons/si";
+
+
 interface projectData {
     name: string,
     url: string
     img: string
 }
+interface serviceData {
+    icon: ReactElement,
+    title: string,
+    desc: string
+}
+
 
 export const projects: projectData[] = [
     {
@@ -31,3 +45,26 @@ export const projects: projectData[] = [
         img: "analog.png"
     },
 ];
+
+export const service: serviceData[] = [
+    {
+        icon: <MdWeb className="text-4xl text-DeepNavy-blue group-hover:text-DeepNavy-blue" />,
+        title: "Custom Website Development",
+        desc: "Crafting unique websites with a focus on responsive design and user-centric experiences."
+    },
+    {
+        icon: <MdAddBusiness className="text-4xl text-DeepNavy-blue group-hover:text-DeepNavy-blue" />,
+        title: "E-commerce & Business Platform",
+        desc: "Building secure and feature-rich e-commerce platforms and robust business platforms."
+    },
+    {
+        icon: <SiWebpack className="text-4xl text-DeepNavy-blue group-hover:text-DeepNavy-blue" />,
+        title: "Content Management Systems (CMS)",
+        desc: "Simplifying content management through user-friendly custom CMS solutions."
+    },
+    {
+        icon: <BsDatabaseFillCheck className="text-4xl text-DeepNavy-blue group-hover:text-DeepNavy-blue" />,
+        title: "API Development & Database Design",
+        desc: "Optimizing applications with robust APIs development and efficient database design"
+    },
+]
